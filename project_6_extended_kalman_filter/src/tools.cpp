c98a553244b3446bb658ec1f3735631e89849cc6
ndrplz <ndrplz@gmail.com>
2017-04-17 16:15:12 +0200

    Implement CalculateRMSE function
    Implemented function for computing root mean squared error in tools.cpp.

--- project_6_extended_kalman_filter/src/tools.cpp
+++ project_6_extended_kalman_filter/src/tools.cpp
@@ -15,6 +15,34 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+	size_t num_estimations = estimations.size();
+	size_t num_ground_truths = ground_truth.size();
+
+	VectorXd rmse(4);
+	rmse << 0, 0, 0, 0;
+
+	// sanity check of input validity
+	if (estimations.size() != ground_truth.size() || estimations.size() < 1) {
+		std::cout << "Cannot compute RMSE metric. Invalid input size.";
+		return rmse;
+	}
+
+	// accumulate residuals
+	for (size_t i = 0; i < estimations.size(); ++i) {
+		VectorXd residual = estimations[i] - ground_truth[i];
+		residual = residual.array() * residual.array();
+		rmse += residual;
+	}
+
+	// compute mean
+	rmse /= estimations.size();
+
+	// compute squared root
+	rmse = rmse.array().sqrt();
+
+	return rmse;
+
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +50,6 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+	Eigen::MatrixXd temp = MatrixXd(2, 2);
+	return temp;
 }
