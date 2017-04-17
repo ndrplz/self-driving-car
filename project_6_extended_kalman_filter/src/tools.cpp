fcf09b817c6a184abba33af91053821503bbe520
ndrplz <ndrplz@gmail.com>
2017-04-17 16:48:51 +0200

    Implement CalculateJacobian function
    Implemented function to calculate Jacobian of measurement function h(x)
    w.r.t. state x.

--- project_6_extended_kalman_filter/src/tools.cpp
+++ project_6_extended_kalman_filter/src/tools.cpp
@@ -11,10 +11,6 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
 
 	size_t num_estimations = estimations.size();
 	size_t num_ground_truths = ground_truth.size();
@@ -24,7 +20,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 
 	// sanity check of input validity
 	if (estimations.size() != ground_truth.size() || estimations.size() < 1) {
-		std::cout << "Cannot compute RMSE metric. Invalid input size.";
+		std::cout << "Cannot compute RMSE metric. Invalid input size." << std::endl;
 		return rmse;
 	}
 
@@ -46,10 +42,31 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
-	Eigen::MatrixXd temp = MatrixXd(2, 2);
-	return temp;
+
+	MatrixXd Hj(3, 4);
+
+	// Unroll state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+	
+	// Pre-compute some term which recur in the Jacobian
+	float c1 = px * px + py * py;
+	float c2 = sqrt(c1);
+	float c3 = c1 * c2;
+
+	// Sanity check to avoid division by zero
+	if (std::abs(c1) < 0.0001) {
+		std::cout << "Error in CalculateJacobian. Division by zero." << std::endl;
+		return Hj;
+	}
+
+	// Actually compute Jacobian matrix
+	Hj << px / c2,					py / c2,					0,			0,
+		-(py / c1),					px / c1,					0,			0,
+		py*(vx*py - vy*px) / c3,	px*(vy*px - vx*py) / c3,	px / c2,	py / c2;
+
+	return Hj;
+
 }
