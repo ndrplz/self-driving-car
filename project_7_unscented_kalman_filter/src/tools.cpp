19ef5183c9a3957b84798d6ab4ba8efe70dd9aeb
ndrplz <ndrplz@gmail.com>
2017-05-07 12:25:47 +0200

    Implement RMSE
    Implementation of Root Mean Squared Error.

--- project_7_unscented_kalman_filter/src/tools.cpp
+++ project_7_unscented_kalman_filter/src/tools.cpp
@@ -10,9 +10,33 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+	const vector<VectorXd> &ground_truth) {
+
+	size_t num_estimations = estimations.size();
+	size_t num_ground_truths = ground_truth.size();
+
+	VectorXd rmse(4);
+	rmse << 0, 0, 0, 0;
+
+	// sanity check of input validity
+	if (estimations.size() != ground_truth.size() || estimations.size() < 1) {
+		std::cout << "Cannot compute RMSE metric. Invalid input size." << std::endl;
+		return rmse;
+	}
+
+	// accumulate residuals
+	for (size_t i = 0; i < estimations.size(); ++i) {
+		VectorXd residual = estimations[i] - ground_truth[i];
+		residual = residual.array() * residual.array();
+		rmse += residual;
+	}
+
+	// compute mean
+	rmse /= estimations.size();
+
+	// compute squared root
+	rmse = rmse.array().sqrt();
+
+	return rmse;
+
 }
