c500094a76c540af69e371771c2a1ba213d1f5d8
ndrplz <ndrplz@gmail.com>
2017-09-04 22:04:36 +0200

    Implement load pretrained vgg

--- project_12_road_segmentation/main.py
+++ project_12_road_segmentation/main.py
@@ -1,8 +1,8 @@
-import os.path
 import tensorflow as tf
 import helper
 import warnings
 from distutils.version import LooseVersion
+from os.path import join
 import project_tests as tests
 
 
@@ -20,26 +20,36 @@ else:
 def load_vgg(sess, vgg_path):
     """
     Load Pretrained VGG Model into TensorFlow.
+
     :param sess: TensorFlow Session
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
+
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+
+    w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    w3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    w4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    w7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
     
-    return None, None, None, None, None
-tests.test_load_vgg(load_vgg, tf)
+    return w1, keep, w3, w4, w7
 
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
     Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
+    For reference: https://people.eecs.berkeley.edu/~jonlong/long_shelhamer_fcn.pdf
+
     :param vgg_layer7_out: TF Tensor for VGG Layer 3 output
     :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
     :param vgg_layer3_out: TF Tensor for VGG Layer 7 output
@@ -48,7 +58,6 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
     # TODO: Implement function
     return None
-tests.test_layers(layers)
 
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
@@ -62,7 +71,6 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
     # TODO: Implement function
     return None, None, None
-tests.test_optimize(optimize)
 
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
@@ -82,7 +90,13 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     """
     # TODO: Implement function
     pass
-tests.test_train_nn(train_nn)
+
+
+def perform_tests():
+    tests.test_load_vgg(load_vgg, tf)
+    # tests.test_layers(layers)
+    # tests.test_optimize(optimize)
+    # tests.test_train_nn(train_nn)
 
 
 def run():
@@ -100,15 +114,18 @@ def run():
     #  https://www.cityscapes-dataset.com/
 
     with tf.Session() as sess:
+
         # Path to vgg model
-        vgg_path = os.path.join(data_dir, 'vgg')
+        vgg_path = join(data_dir, 'vgg')
+
         # Create function to get batches
-        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+        get_batches_fn = helper.gen_batch_function(join(data_dir, 'data_road/training'), image_shape)
 
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        load_vgg(sess, vgg_path)
 
         # TODO: Train NN using the train_nn function
 
@@ -119,4 +136,7 @@ def run():
 
 
 if __name__ == '__main__':
+
+    perform_tests()
+
     run()
